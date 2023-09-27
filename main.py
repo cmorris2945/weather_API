170be29e847a34481d931b7d4202e45ba75e5267
cmorris2945 <cmorris2945@yahoo.com>
2023-09-27 02:24:29 -0400

    added static directory for pictures

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask("AIforGenomics")
+app = Flask(__name__)
 
 ## To connect HTML pages to the "app" instance website use this @app...
-@app.route("/home")
+@app.route("/")
 def home():
-    return render_template("tutorial.html")
-@app.route("/about/")
-def about():
-    return render_template("about.html")
+    return render_template("home.html")
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    #f = pandas.read_csv("")
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
 
